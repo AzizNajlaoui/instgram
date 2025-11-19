61dd89c58c48da91ee22509d8701b24875efbfc0
aziz <medaziznajlaoui@gmail.com>
2025-11-19 18:27:01 +0100

    removed light theme and next themes default theme of instgram is dark and there is no light theme

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { ThemeProvider } from "./theme-provider";
 import { Toaster } from "./ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -9,15 +8,8 @@ const queryClient = new QueryClient();
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-        <Toaster richColors />
-      </ThemeProvider>
+      {children}
+      <Toaster richColors />
     </QueryClientProvider>
   );
 }
